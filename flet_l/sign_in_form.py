92e615a1ca530adf863d0633a8322c07c54b38b9
spike <azamtoiri@gmail.com>
2023-11-06 12:20:56 +0300

    add animations to the fields

--- flet_l/sign_in_form.py
+++ flet_l/sign_in_form.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import flet as ft
 import flet_material as fm
 
@@ -11,31 +13,108 @@ dummy_user_list: list = [["dummy@gmail.com"], 121212]
 
 class CustomInputField(ft.UserControl):
     def __init__(self, password: bool, title: str):
-        self.input: ft.Control = ft.TextField(
+        self.input: ft.TextField = ft.TextField(
             height=50,
+            # few UI properties for the text-fields hard@
+            border_color=ft.colors.PRIMARY,
+            border_width=1,
+            cursor_width=0.5,
+            cursor_color=ft.colors.WHITE10,
+            color=ft.colors.WHITE,
+            text_size=13,
+
+            # bgcolor as per the theme
+            bgcolor=fm.Theme.bgcolor,
+            password=password,
+            on_focus=lambda e: self.focus_shadow(e),
+            on_blur=lambda e: self.blur_shadow(e),
+            on_change=lambda e: self.set_loader_animation(e),
         )
+
         self.input_box: ft.Container = ft.Container(
             expand=True,
             content=self.input,
             animate=ft.Animation(300, ft.animation.AnimationCurve.EASE),
             shadow=None,
         )
+
+        self.loader: ft.ProgressBar = ft.ProgressBar(
+            value=0,
+            bar_height=1.25,
+            color=PRIMARY,
+            bgcolor=ft.colors.TRANSPARENT,
+        )
+
+        self.status: fm.CheckBox = fm.CheckBox(
+            shape="circle",
+            value=False,
+            disabled=True,
+            offset=ft.Offset(1, 0),
+            bottom=0,
+            right=1,
+            top=1,
+            animate_opacity=ft.Animation(200, ft.animation.AnimationCurve.LINEAR),
+            animate_offset=ft.Animation(300, ft.animation.AnimationCurve.EASE),
+            opacity=0,
+        )
+
         self.object = self.create_input(title)
 
         super().__init__()
+
+    async def set_ok(self):
+        self.loader.value = 0
+        self.loader.update()
+
+        self.status.offset = ft.Offset(-0.5, 0)
+        self.status.opacity = 1
+        self.update()
+        await asyncio.sleep(1)
+
+        self.status.content.value = True
+        self.status.animate_checkbox(e=None)
+        self.status.update()
+
+    def set_loader_animation(self, e):
+        # function starts the loader if the text field lengths ore not 0
+        if len(self.input.value) != 0:
+            self.loader.value = None
+        else:
+            self.loader.value = 0
+
+        self.loader.update()
+
+    def focus_shadow(self, e):
+        """Focus shadow when focusing"""
+        self.input.border_color = PRIMARY
+        self.input_box.shadow = ft.BoxShadow(
+            spread_radius=6,
+            blur_radius=8,
+            color=ft.colors.with_opacity(0.25, ft.colors.BLACK),
+            offset=ft.Offset(4, 4)
+        )
+        self.update()
+        # self.set_loader_animation(e=None)
+
+    def blur_shadow(self, e):
+        """ Blur when the textfield loses focus"""
+        self.input_box.shadow = None
+        self.input.border_color = ft.colors.WHITE
+        self.update()
+        # self.set_loader_animation(e=None)
 
     def create_input(self, title):
         return ft.Column(
             spacing=5,
             controls=[
-                ft.Text(title, size=11, weight=ft.FontWeight.BOLD, color="#bbbbbb"),
+                ft.Text(title, size=11, weight=ft.FontWeight.BOLD, color=ft.colors.WHITE),
                 ft.Stack(
                     controls=[
                         self.input_box,
-                        # status...
+                        self.status,
                     ],
-                )
-                # loader ...
+                ),
+                self.loader,
             ],
         )
 
@@ -47,7 +126,7 @@ class CustomInputField(ft.UserControl):
 class MainFormUI(ft.UserControl):
     def __init__(self):
         self.email = CustomInputField(False, "Email")
-        self.password = CustomInputField(False, "Password")
+        self.password = CustomInputField(True, "Password")
         super().__init__()
 
     def build(self):
@@ -68,6 +147,7 @@ class MainFormUI(ft.UserControl):
                     ),
                     ft.Divider(height=25, color=ft.colors.TRANSPARENT),
                     self.email,
+                    ft.Divider(height=5, color=ft.colors.TRANSPARENT),
                     self.password,
                     ft.Divider(height=25, color=ft.colors.TRANSPARENT),
                 ],
