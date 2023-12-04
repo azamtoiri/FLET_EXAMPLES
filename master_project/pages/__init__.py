ca7dd01707a2470d3bb36c8a2e330422c227157c
spike <azamtoiri@gmail.com>
2023-12-04 13:39:38 +0300

    FIXED: imports

--- master_project/pages/__init__.py
+++ master_project/pages/__init__.py
@@ -1,3 +1,4 @@
-from pages.welcome import Welcome
-from pages.login import Login
-from pages.signup import SignUp
+from .welcome import Welcome
+from .login import Login
+from .signup import SignUp
+from .student_page import Student
